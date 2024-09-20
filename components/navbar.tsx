548426f8ecf36142d7e69ba9b77d6a6dea54babe
Raphaelmbewe <rmbewe68@gmail.com>
2024-09-20 10:16:26 +0200

    chore: add code ui

---
+++ components/navbar.tsx
@@ -0,0 +1,133 @@
+"use client";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from "@nextui-org/react";
+import { link as linkStyles } from "@nextui-org/theme";
+import NextLink from "next/link";
+import clsx from "clsx";
+
+import { siteConfig } from "@/config/site";
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+  TwitterIcon,
+  GithubIcon,
+  DiscordIcon,
+  HeartFilledIcon,
+  SearchIcon,
+  Logo,
+} from "@/components/icons";
+
+export const AppNavbar = () => {
+  return (
+    <Navbar maxWidth="full" className="bg-stickyWhite">
+      <NavbarContent className="w-full flex-1">
+        <NavbarBrand>
+          <Logo size={70} />
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-10 w-full flex-1">
+        <NavbarItem>
+          <Link className="text-stickyBlack" href="#" aria-current="page">
+            Pricing
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-stickyBlack" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-stickyBlack" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-stickyBlack" href="#">
+            Blog
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-stickyBlack" href="#">
+            Affiliates
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Dropdown>
+            <NavbarItem>
+              <DropdownTrigger>
+                <Button
+                  disableRipple
+                  className="text-stickyBlack p-0 bg-transparent data-[hover=true]:bg-transparent"
+                  radius="sm"
+                  variant="light"
+                >
+                  Support
+                </Button>
+              </DropdownTrigger>
+            </NavbarItem>
+            <DropdownMenu
+              aria-label="ACME features"
+              className="w-[340px]"
+              itemClasses={{
+                base: "gap-4",
+              }}
+            >
+              <DropdownItem
+                key="autoscaling"
+                description="ACME scales apps to meet user demand, automagically, based on load."
+              >
+                Autoscaling
+              </DropdownItem>
+              <DropdownItem
+                key="usage_metrics"
+                description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+              >
+                Usage Metrics
+              </DropdownItem>
+              <DropdownItem
+                key="production_ready"
+                description="ACME runs on ACME, join us and others serving requests at web scale."
+              >
+                Production Ready
+              </DropdownItem>
+              <DropdownItem
+                key="99_uptime"
+                description="Applications stay on the grid with high availability and high uptime guarantees."
+              >
+                +99% Uptime
+              </DropdownItem>
+              <DropdownItem
+                key="supreme_support"
+                description="Overcome any challenge with a supporting team ready to respond."
+              >
+                +Supreme Support
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent className="w-full flex-1" justify="end">
+        <NavbarItem>
+          <Button
+            as={Link}
+            className="bg-stickyBlack text-stickyWhite"
+            href="#"
+            variant="flat"
+          >
+            Login
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import {nextui} from '@nextui-org/theme'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+      colors: {
+        stickyWhite: '#ECEBE5',
+        stickyBlack: '#27211E',
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
